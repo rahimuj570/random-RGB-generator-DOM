53944fba7e068e2d712fd3d834dfc64c032858f9
rahimuj570 <rahimuj570@gmail.com>
2022-02-19 22:39:24 +0600

    js function added

--- rgb.js
+++ rgb.js
@@ -1,1 +1,20 @@
-document.getElementById("generate-btn").addEventListener("click", () => {});
+document.getElementById("generate-btn").addEventListener("click", function () {
+  const red = rgbGenerate();
+  const green = rgbGenerate();
+  const blue = rgbGenerate();
+  const rgbCode = `rgb(${red},${green},${blue})`;
+  query("body").style.background = rgbCode;
+  query("#rgb-field").value = rgbCode;
+});
+
+// ==========RGB Generator=========
+function rgbGenerate(color) {
+  const randomNum = Math.random() * 255;
+  const float2Int = Math.round(randomNum);
+  return float2Int + "";
+}
+
+//===========Query Selector==========
+query = (idClass) => {
+  return document.querySelector(idClass);
+};
